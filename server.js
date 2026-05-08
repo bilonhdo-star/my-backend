83800d9691bbb2463ad537222619c56c872be818
bilonhdo-star <bilonhdo@gmail.com>
2026-05-09 03:50:12 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,61 @@
 const express = require("express");
-const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+let waitingUser = null;
 
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+
+  console.log("User connected");
+
+  // JOIN CHAT
+  socket.on("join", () => {
+
+    if (waitingUser) {
+      socket.partner = waitingUser;
+      waitingUser.partner = socket;
+
+      socket.emit("matched");
+      waitingUser.emit("matched");
+
+      waitingUser = null;
+    } else {
+      waitingUser = socket;
+    }
+  });
+
+  // MESSAGE
+  socket.on("message", (msg) => {
+    if (socket.partner) {
+      socket.partner.emit("message", msg);
+    }
+  });
+
+  // NEXT
+  socket.on("next", () => {
+    socket.partner = null;
+    waitingUser = socket;
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.partner) {
+      socket.partner.emit("partner-left");
+    }
+  });
+
+});
+
+server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
